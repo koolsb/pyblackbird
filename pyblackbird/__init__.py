504e136d9703f12c59fd34d614fb8b52bbb0b7fe
Ben Kools <koolsb@my.uwstout.edu>
2018-03-21 16:36:12 -0500

    Add support for locking buttons and setting all outputs to single source

--- pyblackbird/__init__.py
+++ pyblackbird/__init__.py
@@ -8,293 +8,323 @@ from serial_asyncio import create_serial_connection
 from threading import RLock
 
 _LOGGER = logging.getLogger(__name__)
-ZONE_PATTERN_ON = re.compile('\D\D\D\s(\d\d)\D\D(\d\d)\s\s\D\D\D\s(\d\d)\D\D\d\d\s')
-ZONE_PATTERN_OFF = re.compile('\D\D\DOFF\D\D(\d\d)\s\s\D\D\D\D\D\D\D\D\d\d\s')
+ZONE_PATTERN_ON = re.compile('\D\D\D\s(\d\d)\D\D\d\d\s\s\D\D\D\s(\d\d)\D\D\d\d\s')
+ZONE_PATTERN_OFF = re.compile('\D\D\DOFF\D\D\d\d\s\s\D\D\D\D\D\D\D\D\d\d\s')
 EOL = b'\r'
 LEN_EOL = len(EOL)
-TIMEOUT = 5 # Number of seconds before serial operation timeout
-SYSTEM_POWER = True
+TIMEOUT = 2 # Number of seconds before serial operation timeout
+SYSTEM_POWER = None
 
 class ZoneStatus(object):
-	def __init__(self,
-				 power: bool,
-				 av: int,
-				 zone: int,
-				 ir: int):
-		self.power = power
-		self.av = av
-		self.zone = zone
-		self.ir = ir
+    def __init__(self,
+                 zone: int,
+                 power: bool,
+                 av: int,
+                 ir: int):
+        self.zone = zone
+        self.power = power
+        self.av = av
+        self.ir = ir
 
-	@classmethod
-	def from_string(cls, string: str):
-		if not string:
-			return None
-		match_on = re.search (ZONE_PATTERN_ON, string)
-		if not match_on:
-			match_off = re.search (ZONE_PATTERN_OFF, string)
-			if not match_off:
-				return None
-			return ZoneStatus(None,None,*[int(m) for m in match_off.groups()],None)
-		return ZoneStatus(1,*[int(m) for m in match_on.groups()])
+    @classmethod
+    def from_string(cls, zone: int, string: str):
+        if not string:
+            return None
+        match_on = re.search (ZONE_PATTERN_ON, string)
+        if not match_on:
+            match_off = re.search (ZONE_PATTERN_OFF, string)
+            if not match_off:
+                return None
+            return ZoneStatus(zone,0,None,None)
+        return ZoneStatus(zone,1,*[int(m) for m in match_on.groups()])
 
-	@classmethod
-	def power_off(cls, zone: int):
-		return ZoneStatus(None,zone,None, None)
+class LockStatus(object):
+    def __init__(self,
+                 lock: bool):
+        self.lock = lock
+
+    @classmethod
+    def from_string(cls, string: str):
+        if not string:
+            return None
+        #string = string[7:].rstrip()
+        if string.startswith('System Locked'):
+            return True
+        else:
+            return False
 
 class Blackbird(object):
-	"""
-	Monoprice blackbird amplifier interface
-	"""
+    """
+    Monoprice blackbird amplifier interface
+    """
 
-	def system_power_status(self):
-		"""
-		Get the system power status
-		:return: status of the system power
-		"""
-		raise NotImplemented()
+    def zone_status(self, zone: int):
+        """
+        Get the structure representing the status of the zone
+        :param zone: zone 1..8
+        :return: status of the zone or None
+        """
+        raise NotImplemented()
 
-	def zone_status(self, zone: int):
-		"""
-		Get the structure representing the status of the zone
-		:param zone: zone 1..8
-		:return: status of the zone or None
-		"""
-		raise NotImplemented()
+    def set_zone_power(self, zone: int, power: bool):
+        """
+        Turn zone on or off
+        :param zone: Zone 1-8
+        :param power: True to turn on, False to turn off
+        """
+        raise NotImplemented()
 
-	def set_system_power(self, power: int):
-		"""
-		Turn the system on, off, or to standby
-		:param power: 1=on, 0=off, 3=standby
-		"""
-		raise NotImplemented()
+    def set_zone_source(self, zone: int, source: int):
+        """
+        Set source for zone
+        :param zone: Zone 1-8
+        :param source: integer from 1-8
+        """
+        raise NotImplemented()
 
-	def set_zone_power(self, zone: int, power: bool):
-		"""
-		Turn zone on or off
-		:param zone: Zone 1-8
-		:param power: True to turn on, False to turn off
-		"""
-		raise NotImplemented()
+    def set_all_zone_source(self, source: int):
+        """
+        Set source for all zones
+        :param source: integer from 1-8
+        """
+        raise NotImplemented()
 
-	def set_source(self, zone: int, source: int):
-		"""
-		Set source for zone
-		:param zone: Zone 1-8
-		:param source: integer from 1-8
-		"""
-		raise NotImplemented()
+    def lock_front_buttons():
+        """
+        Lock front panel buttons
+        """
+        raise NotImplemented()
+
+    def unlock_front_buttons():
+        """
+        Unlock front panel buttons
+        """
+        raise NotImplemented()
+
+    def lock_status():
+        """
+        Report system locking status
+        """
+        raise NotImplemented()
+
 
 # Helpers
 
 def _format_zone_status_request(zone: int) -> bytes:
-	return 'Status{}.\r'.format(zone).encode()
-
-def _format_system_power_status_request() -> bytes:
-	return '%9962.\r'.encode()
-
-def _format_set_system_power(power: int) -> bytes:
-	if power == 1:
-		SYSTEM_POWER = True
-		return 'PWON.\r'.encode()
-	elif power == 0:
-		SYSTEM_POWER = False
-		return 'PWOFF.\r'.encode()
-	elif power == 2:
-		SYSTEM_POWER = False
-		return 'STANDBY.\r'.encode()
-	else:
-		return '\r'.encode()
+    return 'Status{}.\r'.format(zone).encode()
 
 def _format_set_zone_power(zone: int, power: bool) -> bytes:
-	return '{}{}.\r'.format(zone, '@' if power else '$').encode()
+    return '{}{}.\r'.format(zone, '@' if power else '$').encode()
 
-def _format_set_source(zone: int, source: int) -> bytes:
-	source = int(max(1, min(source,8)))
-	return '{}B{}.\r'.format(source, zone).encode()
+def _format_set_zone_source(zone: int, source: int) -> bytes:
+    source = int(max(1, min(source,8)))
+    return '{}B{}.\r'.format(source, zone).encode()
 
+def _format_set_all_zone_source(source: int) -> bytes:
+    source = int(max(1, min(source,8)))
+    return '{}All.\r'.format(source).encode()
+
+def _format_lock_front_buttons() -> bytes:
+    return '/%Lock;\r'.encode()
+
+def _format_unlock_front_buttons() -> bytes:
+    return '/%Unlock;\r'.encode()
+
+def _format_lock_status() -> bytes:
+    return '%9961.\r'.encode()
 
 
 def get_blackbird(port_url):
-	"""
-	Return synchronous version of Blackbird interface
-	:param port_url: serial port, i.e. '/dev/ttyUSB0'
-	:return: synchronous implementation of Blackbird interface
-	"""
-	lock = RLock()
+    """
+    Return synchronous version of Blackbird interface
+    :param port_url: serial port, i.e. '/dev/ttyUSB0'
+    :return: synchronous implementation of Blackbird interface
+    """
+    lock = RLock()
 
-	def synchronized(func):
-		@wraps(func)
-		def wrapper(*args, **kwargs):
-			with lock:
-				return func(*args, **kwargs)
-		return wrapper
+    def synchronized(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            with lock:
+                return func(*args, **kwargs)
+        return wrapper
 
-	class BlackbirdSync(Blackbird):
-		def __init__(self, port_url):
-			self._port = serial.serial_for_url(port_url, do_not_open=True)
-			self._port.baudrate = 9600
-			self._port.stopbits = serial.STOPBITS_ONE
-			self._port.bytesize = serial.EIGHTBITS
-			self._port.parity = serial.PARITY_NONE
-			self._port.timeout = TIMEOUT
-			self._port.write_timeout = TIMEOUT
-			self._port.open()
+    class BlackbirdSync(Blackbird):
+        def __init__(self, port_url):
+            self._port = serial.serial_for_url(port_url, do_not_open=True)
+            self._port.baudrate = 9600
+            self._port.stopbits = serial.STOPBITS_ONE
+            self._port.bytesize = serial.EIGHTBITS
+            self._port.parity = serial.PARITY_NONE
+            self._port.timeout = TIMEOUT
+            self._port.write_timeout = TIMEOUT
+            self._port.open()
 
-		def _process_request(self, request: bytes, skip=0):
-			"""
-			:param request: request that is sent to the blackbird
-			:param skip: number of bytes to skip for end of transmission decoding
-			:return: ascii string returned by blackbird
-			"""
-			_LOGGER.debug('Sending "%s"', request)
-			# clear
-			self._port.reset_output_buffer()
-			self._port.reset_input_buffer()
-			# send
-			self._port.write(request)
-			self._port.flush()
-			# receive
-			result = bytearray()
-			while True:
-				c = self._port.read(1)
-				if c is None:
-					break
-				if not c:
-					raise serial.SerialTimeoutException(
-						'Connection timed out! Last received bytes {}'.format([hex(a) for a in result]))
-				result += c
-				if len(result) > skip and result [-LEN_EOL:] == EOL:
-					break
-			ret = bytes(result)
-			_LOGGER.debug('Received "%s"', ret)
-			return ret.decode('ascii')
+        def _process_request(self, request: bytes, skip=0):
+            """
+            :param request: request that is sent to the blackbird
+            :param skip: number of bytes to skip for end of transmission decoding
+            :return: ascii string returned by blackbird
+            """
+            _LOGGER.debug('Sending "%s"', request)
+            # clear
+            self._port.reset_output_buffer()
+            self._port.reset_input_buffer()
+            # send
+            self._port.write(request)
+            self._port.flush()
+            # receive
+            result = bytearray()
+            while True:
+                c = self._port.read(1)
+                if c is None:
+                    break
+                if not c:
+                    raise serial.SerialTimeoutException(
+                        'Connection timed out! Last received bytes {}'.format([hex(a) for a in result]))
+                result += c
+                if len(result) > skip and result [-LEN_EOL:] == EOL:
+                    break
+            ret = bytes(result)
+            _LOGGER.debug('Received "%s"', ret)
+            return ret.decode('ascii')
 
-		@synchronized
-		def system_power_status(self):
-			status = self._process_request(_format_system_power_status_request(), skip=5)
-			if status.startswith('PW'):
-				status = status[2:].rstrip()
-			return status.rstrip()
+        @synchronized
+        def zone_status(self, zone: int):
+            # Returns status of a zone
+            return ZoneStatus.from_string(zone, self._process_request(_format_zone_status_request(zone), skip=20))
 
-		@synchronized
-		def zone_status(self, zone: int):
-			# First check if system power is on
-			if SYSTEM_POWER:
-				return ZoneStatus.from_string(self._process_request(_format_zone_status_request(zone), skip=20))
-			else:
-				return ZoneStatus.power_off(zone)
+        @synchronized
+        def set_zone_power(self, zone: int, power: bool):
+            # Set zone power
+            self._process_request(_format_set_zone_power(zone, power))
 
-		@synchronized
-		def set_system_power(self, power: int):
-			# 0 = off, 1 = on, 2 = standby
-			self._process_request(_format_set_system_power(power))
+        @synchronized
+        def set_zone_source(self, zone: int, source: int):
+            # Set zone source
+            self._process_request(_format_set_zone_source(zone, source))
 
-		@synchronized
-		def set_zone_power(self, zone: int, power: bool):
-			self._process_request(_format_set_zone_power(zone, power))
+        @synchronized
+        def set_all_zone_source(self, source: int):
+            # Set all zones to one source
+            self._process_request(_format_set_all_zone_source(source))
 
-		@synchronized
-		def set_source(self, zone: int, source: int):
-			self._process_request(_format_set_source(zone, source))
+        @synchronized
+        def lock_front_buttons(self):
+            # Lock front panel buttons
+            self._process_request(_format_lock_front_buttons())
 
-	return BlackbirdSync(port_url)
+        @synchronized
+        def unlock_front_buttons(self):
+            # Unlock front panel buttons
+            self._process_request(_format_unlock_front_buttons())
+
+        @synchronized
+        def lock_status(self):
+            # Report system locking status
+            return LockStatus.from_string(self._process_request(_format_lock_status()))
+
+    return BlackbirdSync(port_url)
 
 
 @asyncio.coroutine
 def get_async_blackbird(port_url, loop):
-	"""
-	Return asynchronous version of Blackbird interface
-	:param port_url: serial port, i.e. '/dev/ttyUSB0'
-	:return: asynchronous implementation of Blackbird interface
-	"""
+    """
+    Return asynchronous version of Blackbird interface
+    :param port_url: serial port, i.e. '/dev/ttyUSB0'
+    :return: asynchronous implementation of Blackbird interface
+    """
 
-	lock = asyncio.Lock()
+    lock = asyncio.Lock()
 
-	def locked_coro(coro):
-		@asyncio.coroutine
-		@wraps(coro)
-		def wrapper(*args, **kwargs):
-			with (yield from lock):
-				return (yield from coro(*args, **kwargs))
-		return wrapper
+    def locked_coro(coro):
+        @asyncio.coroutine
+        @wraps(coro)
+        def wrapper(*args, **kwargs):
+            with (yield from lock):
+                return (yield from coro(*args, **kwargs))
+        return wrapper
 
-	class BlackbirdAsync(Blackbird):
-		def __init__(self, blackbird_protocol):
-			self._protocol = blackbird_protocol
+    class BlackbirdAsync(Blackbird):
+        def __init__(self, blackbird_protocol):
+            self._protocol = blackbird_protocol
 
-		@locked_coro
-		@asyncio.coroutine
-		def system_power_status(self):
-			status = yield from self._protocol.send(_format_system_power_status_request(), skip=5)
-			if status.startswith('PW'):
-				status = status[2:]
-			return status
+        @locked_coro
+        @asyncio.coroutine
+        def zone_status(self, zone: int):
+            string = yield from self._protocol.send(_format_zone_status_request(zone), skip=15)
+            return ZoneStatus.from_string(zone, string)
 
-		@locked_coro
-		@asyncio.coroutine
-		def zone_status(self, zone: int):
-			# First check if system power is on
-			if SYSTEM_POWER:
-				string = yield from self._protocol.send(_format_zone_status_request(zone), skip=15)
-				return ZoneStatus.from_string(string)
-			else:
-				return ZoneStatus.power_off(zone)
+        @locked_coro
+        @asyncio.coroutine
+        def set_zone_power(self, zone: int, power: bool):
+            yield from self._protocol.send(_format_set_zone_power(zone, power))
 
-		@locked_coro
-		@asyncio.coroutine
-		def set_system_power(self, power: int):
-			yield from self._protocol.send(_format_set_system_power(power))
+        @locked_coro
+        @asyncio.coroutine
+        def set_zone_source(self, zone: int, source: int):
+            yield from self._protocol.send(_format_set_zone_source(zone, source))
 
-		@locked_coro
-		@asyncio.coroutine
-		def set_zone_power(self, zone: int, power: bool):
-			yield from self._protocol.send(_format_set_zone_power(zone, power))
+        @locked_coro
+        @asyncio.coroutine
+        def set_all_zone_source(self, source: int):
+            yield from self._protocol.send(_format_set_all_zone_source(source))
 
-		@locked_coro
-		@asyncio.coroutine
-		def set_source(self, zone: int, source: int):
-			yield from self._protocol.send(_format_set_source(zone, source))
+        @locked_coro
+        @asyncio.coroutine
+        def lock_front_buttons(self):
+            yield from self._protocol.send(_format_lock_front_buttons())
 
-	class BlackbirdProtocol(asyncio.Protocol):
-		def __init__(self, loop):
-			super().__init__()
-			self._loop = loop
-			self._lock = asyncio.Lock()
-			self._transport = None
-			self._connected = asyncio.Event(loop=loop)
-			self.q = asyncio.Queue(loop=loop)
+        @locked_coro
+        @asyncio.coroutine
+        def unlock_front_buttons(self):
+            yield from self._protocol.send(_format_unlock_front_buttons())
 
-		def connection_made(self, transport):
-			self._transport = transport
-			self._connected.set()
-			_LOGGER.debug('port opened %s', self._transport)
+        @locked_coro
+        @asyncio.coroutine
+        def lock_status(self):
+            string = yield from self._protocol.send(_format_lock_status())
+            return LockStatus.from_string(string)
 
-		def data_received(self, data):
-			asyncio.ensure_future(self.q.put(data), loop=self._loop)
+    class BlackbirdProtocol(asyncio.Protocol):
+        def __init__(self, loop):
+            super().__init__()
+            self._loop = loop
+            self._lock = asyncio.Lock()
+            self._transport = None
+            self._connected = asyncio.Event(loop=loop)
+            self.q = asyncio.Queue(loop=loop)
 
-		@asyncio.coroutine
-		def send(self, request: bytes, skip=0):
-			yield from self._connected.wait()
-			result = bytearray()
-			# Only one transaction at a time
-			with (yield from self._lock):
-				self._transport.serial.reset_output_buffer()
-				self._transport.serial.reset_input_buffer()
-				while not self.q.empty():
-					self.q.get_nowait()
-				self._transport.write(request)
-				try:
-					while True:
-						result += yield from asyncio.wait_for(self.q.get(), TIMEOUT, loop=self._loop)
-						if len(result) > skip and result[-LEN_EOL:] == EOL:
-							ret = bytes(result)
-							_LOGGER.debug('Received "%s"', ret)
-							return ret.decode('ascii')
-				except asyncio.TimeoutError:
-					_LOGGER.error("Timeout during receiving response for command '%s', received='%s'", request, result)
-					raise
+        def connection_made(self, transport):
+            self._transport = transport
+            self._connected.set()
+            _LOGGER.debug('port opened %s', self._transport)
 
-	_, protocol = yield from create_serial_connection(loop, functools.partial(BlackbirdProtocol, loop), port_url, baudrate=9600)
+        def data_received(self, data):
+            asyncio.ensure_future(self.q.put(data), loop=self._loop)
 
-	return BlackbirdAsync(protocol)
+        @asyncio.coroutine
+        def send(self, request: bytes, skip=0):
+            yield from self._connected.wait()
+            result = bytearray()
+            # Only one transaction at a time
+            with (yield from self._lock):
+                self._transport.serial.reset_output_buffer()
+                self._transport.serial.reset_input_buffer()
+                while not self.q.empty():
+                    self.q.get_nowait()
+                self._transport.write(request)
+                try:
+                    while True:
+                        result += yield from asyncio.wait_for(self.q.get(), TIMEOUT, loop=self._loop)
+                        if len(result) > skip and result[-LEN_EOL:] == EOL:
+                            ret = bytes(result)
+                            _LOGGER.debug('Received "%s"', ret)
+                            return ret.decode('ascii')
+                except asyncio.TimeoutError:
+                    _LOGGER.error("Timeout during receiving response for command '%s', received='%s'", request, result)
+                    raise
+
+    _, protocol = yield from create_serial_connection(loop, functools.partial(BlackbirdProtocol, loop), port_url, baudrate=9600)
+
+    return BlackbirdAsync(protocol)
--- tests/test_blackbird.py
+++ tests/test_blackbird.py
@@ -9,72 +9,82 @@ import asyncio
 
 class TestZoneStatus(unittest.TestCase):
 
-	def test_zone_status_broken(self):
-		self.assertIsNone(ZoneStatus.from_string(None))
-		self.assertIsNone(ZoneStatus.from_string('VA: 09-<01\r'))
-		self.assertIsNone(ZoneStatus.from_string('\r\n\r\n'))
+    def test_zone_status_broken(self):
+        self.assertIsNone(ZoneStatus.from_string(None, None))
+        self.assertIsNone(ZoneStatus.from_string(1, 'VA: 09-<01\r'))
+        self.assertIsNone(ZoneStatus.from_string(10, '\r\n\r\n'))
 
 class TestBlackbird(unittest.TestCase):
-	def setUp(self):
-		self.responses = {}
-		self.blackbird = get_blackbird(create_dummy_port(self.responses))
+    def setUp(self):
+        self.responses = {}
+        self.blackbird = get_blackbird(create_dummy_port(self.responses))
 
-	def test_system_power_status(self):
-		self.responses[b'%9962.\r'] = b'PWON \r'
-		status = self.blackbird.system_power_status()
-		self.assertEqual(0, len(self.responses))
+    def test_zone_status(self):
+        self.responses[b'Status1.\r'] = b'AV: 02->01\r\nIR: 02->01\r'
+        status = self.blackbird.zone_status(1)
+        self.assertEqual(1, status.zone)
+        self.assertTrue(status.power)
+        self.assertEqual(2, status.av)
+        self.assertEqual(2, status.ir)
+        self.assertEqual(0, len(self.responses))
 
-	def test_zone_status(self):
-		self.responses[b'Status1.\r'] = b'AV: 02->01\r\nIR: 02->01\r'
-		self.responses[b'%9962.\r'] = b'PWON \r'
-		status = self.blackbird.zone_status(1)
-		self.assertEqual(1, status.zone)
-		self.assertTrue(status.power)
-		self.assertEqual(2, status.av)
-		self.assertEqual(2, status.ir)
 
-	def test_set_system_power(self):
-		self.responses[b'PWON.\r'] = b'PWON\r'
-		self.blackbird.set_system_power(1)
-		self.responses[b'PWOFF.\r'] = b'PWOFF\r'
-		self.blackbird.set_system_power(0)
-		self.responses[b'STANDBY.\r'] = b'STANDBY\r'
-		self.blackbird.set_system_power(2)
-		self.responses[b'\r'] = b'\r'
-		self.blackbird.set_system_power(3)
-		self.assertEqual(0, len(self.responses))
+    def test_set_zone_power(self):
+        self.responses[b'1@.\r'] = b'01 Open.\r'
+        self.blackbird.set_zone_power(1, True)
+        self.responses[b'1@.\r'] = b'01 Open.\r'
+        self.blackbird.set_zone_power(1, 'True')
+        self.responses[b'1@.\r'] = b'01 Open.\r'
+        self.blackbird.set_zone_power(1, 1)
+        self.responses[b'1$.\r'] = b'01 Closed.\r'
+        self.blackbird.set_zone_power(1, False)
+        self.responses[b'1$.\r'] = b'01 Closed.\r'
+        self.blackbird.set_zone_power(1, None)
+        self.responses[b'1$.\r'] = b'01 Closed.\r'
+        self.blackbird.set_zone_power(1, 0)
+        self.responses[b'1$.\r'] = b'01 Closed.\r'
+        self.blackbird.set_zone_power(1, '')
+        self.assertEqual(0, len(self.responses))
 
-	def test_set_zone_power(self):
-		self.responses[b'1@.\r'] = b'01 Open.\r'
-		self.blackbird.set_zone_power(1, True)
-		self.responses[b'1@.\r'] = b'01 Open.\r'
-		self.blackbird.set_zone_power(1, 'True')
-		self.responses[b'1@.\r'] = b'01 Open.\r'
-		self.blackbird.set_zone_power(1, 1)
-		self.responses[b'1$.\r'] = b'01 Closed.\r'
-		self.blackbird.set_zone_power(1, False)
-		self.responses[b'1$.\r'] = b'01 Closed.\r'
-		self.blackbird.set_zone_power(1, None)
-		self.responses[b'1$.\r'] = b'01 Closed.\r'
-		self.blackbird.set_zone_power(1, 0)
-		self.responses[b'1$.\r'] = b'01 Closed.\r'
-		self.blackbird.set_zone_power(1, '')
-		self.assertEqual(0, len(self.responses))
+    def test_set_zone_source(self):
+        self.responses[b'1B1.\r'] = b'AV:01->01\r'
+        self.blackbird.set_zone_source(1,1)
+        self.responses[b'8B1.\r'] = b'AV:08->05\r'
+        self.blackbird.set_zone_source(1,100)
+        self.responses[b'1B1.\r'] = b'AV:01->01\r'
+        self.blackbird.set_zone_source(1,-100)
+        self.responses[b'2B2.\r'] = b'AV:02->02\r'
+        self.blackbird.set_zone_source(2,2)
+        self.assertEqual(0, len(self.responses))
 
-	def test_set_source(self):
-		self.responses[b'1B1.\r'] = b'AV:01->01\r'
-		self.blackbird.set_source(1,1)
-		self.responses[b'8B1.\r'] = b'AV:08->05\r'
-		self.blackbird.set_source(1,100)
-		self.responses[b'1B1.\r'] = b'AV:01->01\r'
-		self.blackbird.set_source(1,-100)
-		self.responses[b'2B2.\r'] = b'AV:02->02\r'
-		self.blackbird.set_source(2,2)
-		self.assertEqual(0, len(self.responses))
+    def test_set_all_zone_source(self):
+        self.responses[b'1All.\r'] = b'01 To All.\r'
+        self.blackbird.set_all_zone_source(1)
+        self.assertEqual(0, len(self.responses))
 
-	def test_timeout(self):
-		with self.assertRaises(serial.SerialTimeoutException):
-			self.blackbird.set_source(6,6)
+    def test_lock_front_buttons(self):
+        self.responses[b'/%Lock;\r'] = b'System Locked!\r'
+        self.blackbird.lock_front_buttons()
+        self.assertEqual(0, len(self.responses))
+
+
+    def test_unlock_front_buttons(self):
+        self.responses[b'/%Unlock;\r'] = b'System UnLock!\r'
+        self.blackbird.unlock_front_buttons()
+        self.assertEqual(0, len(self.responses))
+
+    def test_front_button_status(self):
+        self.responses[b'%9961.\r'] = b'System Locked!\r'
+        status = self.blackbird.lock_status()
+        self.assertTrue(status)
+        self.responses[b'%9961.\r'] = b'System UnLock!\r'
+        status = self.blackbird.lock_status()
+        self.assertFalse(status)
+        self.assertEqual(0, len(self.responses))
+
+    def test_timeout(self):
+        with self.assertRaises(serial.SerialTimeoutException):
+           self.blackbird.set_zone_source(6,6)
 
 
 
@@ -95,7 +105,7 @@ class TestAsyncBlackbird(TestBlackbird):
 
     def test_timeout(self):
         with self.assertRaises(asyncio.TimeoutError):
-            self.blackbird.set_source(6, 6)
+            self.blackbird.set_zone_source(6, 6)
 
 if __name__ == '__main__':
-	unittest.main()
+   unittest.main()
